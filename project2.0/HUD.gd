2831ad73c8fe7b2e8f68bfd1d6505e19811ca731
ayush1014 <60238194+ayush1014@users.noreply.github.com>
2022-01-31 11:02:32 -0600

    game1, dodge the creep

---
+++ project2.0/HUD.gd
@@ -0,0 +1,33 @@
+extends CanvasLayer
+
+signal start_game
+
+func show_message(text):
+	$Message.text = text
+	$Message.show()
+	$MessageTimer.start()
+	
+func show_game_over(): 
+	show_message("Game Over")
+	yield($MessageTimer, "timeout")
+	
+	$Message.text = "Dodge the \nCreeps!"
+	$Message.show()
+	get_tree().create_timer(1)
+	
+	yield (get_tree().create_timer(1), "timeout")
+	$StartButton.show()
+	
+func update_score(score):
+	$ScoreLable.text = str(score)
+	
+
+func _on_MessageTimer_timeout():
+	$Message.hide()
+
+
+func _on_StartButton_pressed():
+	$StartButton.hide()
+	emit_signal("Start Game")
+	
+	
